007babd85c0b70e075a65d0935b77bc745000272
Jan Vermaelen <jan.vermaelen@cs.kuleuven.be>
2021-10-27 08:43:38 +0200

    no H drive

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="H:\test_project\venv"
+$env:VIRTUAL_ENV="C:\Workdir\BvP\test_project\opgave\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
