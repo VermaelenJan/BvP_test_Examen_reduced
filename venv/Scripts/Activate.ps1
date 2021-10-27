7f6ef0887930ab9059373c64812206e5fed74a0e
Jan Vermaelen <jan.vermaelen@cs.kuleuven.be>
2021-10-27 08:05:42 +0200

    changed activate files

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\u0132380\documents\Opgave\venv"
+$env:VIRTUAL_ENV="H:\test_project\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
